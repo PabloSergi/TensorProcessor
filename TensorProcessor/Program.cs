26e085bb460936c98142426c74923fc08da967a2
PabloSergi <106373652+PabloSergi@users.noreply.github.com>
2023-04-26 00:19:52 +0700

    -1

--- TensorProcessor/Program.cs
+++ TensorProcessor/Program.cs
@@ -1,101 +1,70 @@
 ﻿using System;
-using System.IO;
-using TorchSharp;
-using static TorchSharp.torch;
-using Tensorflow;
-using Accord.MachineLearning;
 using Accord.Math.Distances;
+using Accord.MachineLearning;
+using TorchSharp;
+using System.IO;
+using Accord.IO;
 
-partial class Program
+
+namespace TensorProcessor
 {
-
-    static void Main(string[] args)
+    class Program
     {
-        if (args.Length == 0)
+        public static int[] ClusterEmbeddings(double[][] embeddings, int numClusters)
         {
-            Console.WriteLine("Usage: TensorProcessor /Users/pablo_sergi/dockercerts/embeddings.pt");
-            return;
-        }
-
-        string filePath = args[0];
-
-        if (!File.Exists(filePath))
-        {
-            Console.WriteLine($"File '{filePath}' not found.");
-            return;
-        }
-
-        double[][] inputTensors = LoadTensorFromFileAndConvertToDoubleArray(filePath);
-
-        PrintDoubleArray(inputTensors);
-
-
-        int numClusters = 2; // Укажите количество кластеров
-        int[] clusterLabels = ClusterEmbeddings(inputTensors, numClusters);
-
-        // Вывод меток кластеров на консоль
-        Console.WriteLine("Метки кластеров:");
-        for (int i = 0; i < clusterLabels.Length; i++)
-        {
-            Console.Write(clusterLabels[i] + " ");
-        }
-        Console.WriteLine();
-
-    }
-
-    static double[][] LoadTensorFromFileAndConvertToDoubleArray(string filePath)
-    {
-        var tensor = torch.Tensor.load(filePath);
-
-        long[] shape = tensor.shape;
-        long numRows = shape[0];
-        long numCols = shape[1];
-
-        double[][] result = new double[numRows][];
-
-        for (int i = 0; i < numRows; i++)
-        {
-            result[i] = new double[numCols];
-
-            for (int j = 0; j < numCols; j++)
+            var kmeans = new KMeans(numClusters, new Euclidean())
             {
-                result[i][j] = tensor[i, j].ToDouble();
+                Tolerance = 1e-5,
+                MaxIterations = 100,
+            };
+
+            var clusterLabels = kmeans.Learn(embeddings);
+            int[] labels = clusterLabels.Decide(embeddings);
+            return labels;
+        }
+
+        public static double[][] LoadEmbeddings(string filePath)
+        {
+            var tensor = torch.load(filePath);
+            var shape = tensor.shape;
+            long rows = shape[0];
+            long cols = shape[1];
+
+            double[][] embeddings = new double[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                embeddings[i] = new double[cols];
+                for (int j = 0; j < cols; j++)
+                {
+                    embeddings[i][j] = tensor[i, j].ToDouble();
+                }
             }
+
+            return embeddings;
         }
 
-        return result;
-    }
 
-    static void PrintDoubleArray(double[][] array)
-    {
-        for (int i = 0; i < array.Length; i++)
+        static void Main(string[] args)
         {
-            for (int j = 0; j < array[i].Length; j++)
+            //if (args.Length == 0)
+            //{
+            //    Console.WriteLine("Пожалуйста, укажите путь к файлу с эмбеддингами.");
+            //    return;
+            //}
+
+            string filePath = "/Users/pablo_sergi/dockercerts/embeddings.pt"; //args[0];
+            double[][] embeddings = LoadEmbeddings(filePath);
+
+            int numClusters = 2;
+            int[] clusterLabels = ClusterEmbeddings(embeddings, numClusters);
+
+            Console.WriteLine("Метки кластеров:");
+            for (int i = 0; i < clusterLabels.Length; i++)
             {
-                Console.Write($"{array[i][j]} ");
+                Console.Write(clusterLabels[i] + " ");
             }
             Console.WriteLine();
         }
     }
-
-
-    public static int[] ClusterEmbeddings(double[][] embeddings, int numClusters)
-    {
-
-        var kmeans = new KMeans(numClusters, new Euclidean())
-        {
-
-            Tolerance = 1e-5, 
-            MaxIterations = 100,
-        };
-
-
-        var clusterLabels = kmeans.Learn(embeddings);
-
-
-        int[] labels = clusterLabels.Decide(embeddings);
-
-        return labels;
-    }
-
 }
